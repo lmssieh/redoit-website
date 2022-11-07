440ec85337939838431dbfca0db7e58ac87f61bd
rust <53446488+lmssieh@users.noreply.github.com>
2022-11-07 21:12:24 +0100

    change color on hover

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
 	<nav class="flex justify-between items-center p-4 max-w-screen-lg m-auto">
-		<h1 class="text-xl font-semibold">
+		<h1 class="text-xl font-semibold hover:(text-green-100 underline)">
 			<NuxtLink href="/">REDOIT!</NuxtLink>
 		</h1>
 		<a
-			href="#"
+			href="https://web.redoit.app/"
 			class="px-4 py-2 md:( font-semibold text-lg) text-green-100 underline inline-block rounded-lg"
 			>go to app ↗︎</a
 		>
