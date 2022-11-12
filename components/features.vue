bb9a8cbf37b001268ca56aa4ed889da5c10d5353
rust <53446488+lmssieh@users.noreply.github.com>
2022-11-12 11:08:14 +0100

    fix: update text

--- components/features.vue
+++ components/features.vue
@@ -1,77 +1,78 @@
 <template>
-	<div class=" font-poppins">
-		<div>
-			<div class="text-center pt-5 pb-15">
-				<h2 class="text-xl md:(text-2xl leading-13) font-bold">
-					Everything you need to reach your goals.
-				</h2>
-			</div>
-			<div
-				class="md:(grid grid-cols-3 text-center gap-x-4 gap-y-8) children:(flex-1 mx-auto py-4)"
-			>
-				<div class="max-w-[300px]">
-					<div
-						class="bg-blue-100 text-blue-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
-					>
-						<Icon name="heroicons-solid:trending-up" size="50%" />
-					</div>
-					<h3 class="font-semibold py-5">Enjoy your progress</h3>
-					<div>
-						Keep track of your personal growth and see how tiny steps lead you
-						to big results.
-					</div>
-				</div>
-				<div class="max-w-[300px]">
-					<div
-						class="bg-red-100 text-red-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
-					>
-						<Icon name="fluent:skip-forward-tab-20-filled" size="50%" />
-					</div>
-					<h3 class="font-semibold py-5">Unlimited skips</h3>
-					<div>
-						Going on vacation or taking a day off? Skip habits without breaking
-						your chain.
-					</div>
-				</div>
-				<div class="max-w-[300px]">
-					<div
-						class="bg-yellow-100 text-yellow-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
-					>
-						<Icon name="heroicons-outline:lightning-bolt" size="50%" />
-					</div>
-					<h3 class="font-semibold py-5">Simple and fast</h3>
-					<div>
-						A few seconds daily is all it takes to track your routines with
-						redoit! unique color-coded system.
-					</div>
-				</div>
-
-			</div>
-				<div class="md:(flex justify-center gap-x-4) children:(mx-auto py-8 ) md:text-center">
-
-				<div class="max-w-[300px]">
-					<div
-						class="bg-purple-100 text-purple-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
-					>
-						<Icon name="akar-icons:schedule" size="50%" />
-					</div>
-					<h3 class="font-semibold py-5">Get reminded</h3>
-					<div>
-						Our powerful and flexible reminders make it extremely easy to start a new habit!					</div>
-				</div>
-				<div class="max-w-[300px]">
-					<div
-						class="bg-orange-100 text-orange-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
-					>
-						<Icon name="ic:outline-cloud-sync" size="50%" />
-					</div>
-					<h3 class="font-semibold py-5">Multi-device Syncing</h3>
-					<div>
-						Our powerful and flexible reminders make it extremely easy to start a new habit!					</div>
-				</div>
-				</div>
-
-
-		</div>
-	</div>
+  <div class="font-poppins">
+    <div>
+      <div class="text-center pt-5 pb-15">
+        <h2 class="text-xl md:(text-2xl leading-13) font-bold">
+          Everything you need to reach your goals.
+        </h2>
+      </div>
+      <div
+        class="md:(grid grid-cols-3 text-center gap-x-4 gap-y-8) children:(flex-1 mx-auto py-4)"
+      >
+        <div class="max-w-[300px]">
+          <div
+            class="bg-blue-100 text-blue-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
+          >
+            <Icon name="heroicons-solid:trending-up" size="50%" />
+          </div>
+          <h3 class="font-semibold py-5">Enjoy your progress</h3>
+          <div>
+            Keep track of your personal growth and see how tiny steps lead you
+            to big results.
+          </div>
+        </div>
+        <div class="max-w-[300px]">
+          <div
+            class="bg-red-100 text-red-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
+          >
+            <Icon name="fluent:skip-forward-tab-20-filled" size="50%" />
+          </div>
+          <h3 class="font-semibold py-5">Unlimited skips</h3>
+          <div>
+            Going on vacation or taking a day off? Skip habits without breaking
+            your chain.
+          </div>
+        </div>
+        <div class="max-w-[300px]">
+          <div
+            class="bg-yellow-100 text-yellow-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
+          >
+            <Icon name="heroicons-outline:lightning-bolt" size="50%" />
+          </div>
+          <h3 class="font-semibold py-5">Simple and fast</h3>
+          <div>
+            A few seconds daily is all it takes to track your routines with
+            redoit! unique color-coded system.
+          </div>
+        </div>
+      </div>
+      <div
+        class="md:(flex justify-center gap-x-4) children:(mx-auto py-8 ) md:text-center"
+      >
+        <div class="max-w-[300px]">
+          <div
+            class="bg-purple-100 text-purple-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
+          >
+            <Icon name="akar-icons:schedule" size="50%" />
+          </div>
+          <h3 class="font-semibold py-5">Get reminded</h3>
+          <div>
+            Our powerful and flexible reminders make it extremely easy to start
+            a new habit!
+          </div>
+        </div>
+        <div class="max-w-[300px]">
+          <div
+            class="bg-orange-100 text-orange-400 rounded-1xl h-[100px] w-[100px] inline-flex justify-center items-center md:(h-[100px] w-[100px] rounded-3xl)"
+          >
+            <Icon name="ic:outline-cloud-sync" size="50%" />
+          </div>
+          <h3 class="font-semibold py-5">Multi-device Syncing</h3>
+          <div>
+            your data is always synchronized and accessible on every device.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
